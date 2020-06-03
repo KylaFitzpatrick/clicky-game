42b064ded0b050451bf1401819d6a9a36b47a5bf
fitzpaky <kfitzpatrick44@gmail.com>
2020-06-03 12:01:55 -0700

    adding image size

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -6,7 +6,7 @@ function Hero(props) {
     <div class="parallax-container">
         <h1>Clicky Game</h1>
         <h2>Click an image to test your memory and earn points!</h2>
-    <div class="parallax"><img alt={props.name} src="https://images.pexels.com/photos/2521619/pexels-photo-2521619.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img></div>
+    <div class="parallax"><img alt={props.name} src="https://images.pexels.com/photos/2521619/pexels-photo-2521619.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=500&w=1010"></img></div>
   </div>
    
   );
